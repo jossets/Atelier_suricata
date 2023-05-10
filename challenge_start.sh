00e27652431357d6f6c7d2155347942dc6042cd5
jossets <13644560+jossets@users.noreply.github.com>
2023-05-10 14:33:47 +0200

    maj

--- challenge_start.sh
+++ challenge_start.sh
@@ -2,5 +2,12 @@
 # 
 #
 
-./go_create_lan.sh
-cd lab && docker-compose up -d
+
+
+./lan_create.sh && \
+cd lab && docker-compose build && \
+cd ../suricata && docker-compose build && \
+cd ../lab && docker-compose down  
+docker-compose rm -f 
+docker rm ctf-sqli_attack ctf-sqli ctf-sqli_php ctf-sqli_mysql suricata3
+docker-compose up -d
